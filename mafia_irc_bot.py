883a41b125bb34df35fde22a65cc6696dcf94185
Nicholas Brown <nichwn@gmail.com>
2014-01-09 12:39:51 +1100

    Debugged those commands, and added IRC connectivity.

--- mafia_irc_bot.py
+++ mafia_irc_bot.py
@@ -1,6 +1,8 @@
-#TODO - override notices?
-#TODO - !help or /msg HELP etc.
-#TODO - check getEtc. rather than etc.
+# TODO - override notices?
+# TODO - !help or /msg HELP etc.
+# TODO - check getEtc. rather than etc.
+# TODO - manage if a player d/cs or is kicked
+# TODO - allow for reconnection? Reset game? Continue game but check integrity?
 
 """
 
@@ -11,97 +13,143 @@ An IRC bot which manages Mafia games.
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
 
+import sys
+
 
 class Role:
     """Stores role information."""
 
-    def __init__(self, r_name, alignment, ab_active, gop):
+    def __init__(self, r_name, alignment, ab_active):
         self.r_name = r_name
         self.alignment = alignment
         self.ab_active = ab_active
-        self.gop = gop
         self.ab_target = None
 
 
 class Player:
     """Stores player information."""
 
-    def __init__(self, status, role):
-        self.status = status
+    def __init__(self, state = "alive", role = None):
+        self.state = state
         self.role = role
-        self.vote = []
+        self.vote = ""
         self.voted_by = []
 
 
 class MafiaGame:
     """Handles the mafia game."""
 
-    player_list = []
-    player_data = []
-
-    #Flags for game states
+    # Flags for game states
     INITIAL = 0
     SIGN_UP = 1
     DAY = 2
     NIGHT = 3
 
-    state = INITIAL
 
+    def __init__(self):
+        self.players = {}
+        self.gop = ""
+        self.phase = self.INITIAL
 
-    def newGame(self):
+    def newGame(self, user):
         """Start a new game."""
-        self.state = SIGN_UP
+        self.phase = self.SIGN_UP
+        self.gop = user
+        self.players[user] = None
 
     def commands(self):
         """Returns a list of currently available commands and how to use
         them."""
-        #TODO - implement
+        # TODO - implement
         pass
 
-    def getPhase():
-        """Returns a flag that indicates the current phase."""
-        return self.status
+    def getPhase(self):
+        """Returns a flag that indicates the current phase. It's guaranteed
+        that the flag values are in the order of:
 
-    def getSign_Up():
-        """Returns a flag that indicates the initial phase."""
+        Initial < Sign_Up < Day < Night"""
+        return self.phase
+
+    def getInitial(self):
+        """Returns a flag that indicates the initial phase. It's guaranteed
+        that the flag values are in the order of:
+
+        Initial < Sign_Up < Day < Night"""
         return self.INITIAL
 
-    def getSign_Up():
-        """Returns a flag that indicates the sign_up phase."""
+    def getSign_Up(self):
+        """Returns a flag that indicates the sign_up phase. It's guaranteed
+        that the flag values are in the order of:
+
+        Initial < Sign_Up < Day < Night"""
         return self.SIGN_UP
 
-    def getSign_Up():
-        """Returns a flag that indicates the day phase."""
+    def getDay(self):
+        """Returns a flag that indicates the day phase. It's guaranteed
+        that the flag values are in the order of:
+
+        Initial < Sign_Up < Day < Night"""
         return self.DAY
 
-    def getSign_Up():
-        """Returns a flag that indicates the night phase."""
+    def getNight(self):
+        """Returns a flag that indicates the night phase. It's guaranteed
+        that the flag values are in the order of:
+
+        Initial < Sign_Up < Day < Night"""
         return self.NIGHT
 
-    def end(user):
-        pass
-        #TODO - implement
+    def end(self, user):
+        """Attempts to end the game. Return True if the game was ended."""
+        if user == self.gop:
+            self.gameClear()
+            return True
+        else:
+            return False
+
+    def gameClear(self):
+        """Clears the game data in preparation for a new game."""
+        self.players = {}
+        self.phase = self.INITIAL
+
+    def transferGop(self, user, target):
+        """Transfer GOP to a target player."""
+        print target, self.players.keys()
+        if user == self.gop and target in self.players:
+            self.gop = target
+            return True
+        else:
+            return False
+
+    def getLivingPlayers(self):
+        """Return a list of all living players."""
+        alive = []
+        for k, v in self.players.iteritems():
+            # Iterate through all the players
+            if v.state == "alive":
+                # Living player found
+                alive.append(k)
+        return alive
 
 
-class MafiaBot:
+class MafiaBot(irc.IRCClient):
     """A bot which manages a mafia game."""
 
-    nickname = "Mafiabot"
+    nickname = "Mafiabot"  # TODO - make an argument parameter
+    password = "mafiabot"  # TODO - make an argument parameter
     sourceURL = ""  # TODO - fill in later
-    
-    game = MafiaGame()
 
 
-    #Connection
+    # Connection
 
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
+        self.game = MafiaGame()
 
     def connectionLost(self, reason):
         irc.IRCClient.connectionLost(self, reason)
 
-    
-    #Events
+
+    # Events
 
     def signedOn(self):
         """After connecting to the server, join the channel."""
@@ -115,46 +163,56 @@ class MafiaBot:
 
     def privmsg(self, user, channel, msg):
         """Parse and interpret commands."""
-        user = user.split('!', 1)
 
-        #Check if it is a command
+        # Usernames are stored in lowercase, but the original input needs to be
+        # kept for message outputs
+        user_orig = user.split('!', 1)[0]
+        user = user_orig.lower()
+
+        # Check if it is a command
         if msg.startswith('!'):
-            command = msg[1:].lower()
+            command_orig = msg[1:]
         elif msg.startswith(user + ": !"):
-            command = msg[len(user) + 4:].lower()
+            command_orig = msg[len(user) + 4:]
         elif msg.startswith(user + ":!"):
-            command = msg[len(user) + 3].lower()
+            command_orig = msg[len(user) + 3]
         else:
             return;
 
-        #Interpret command
+        # For commands where parameters may be outputted, the original input
+        # needs to be kept
+        command = command_orig.lower()
+        
+        # Interpret command
         if command == "new":
-            #Attempt to start a new game
-            if channel != self.nickname and self.game.status == INITIAL:
-                #Start a new game
-                self.game.newGame()
-                msg = ("A new mafia game has begun! Type !join to join. There is "
-                       "minimum of 5, and a maximum of 12 players that can play.\n "
-                       "The game starter has been given Game Operator (GOP) status "
-                       "and can commence the game by typing !start")
-                
-            elif self.game.status != INITIAL:
-                #Can't start a new game as one is already in progress
+            # Attempt to start a new game
+            if (channel != self.nickname.lower() and
+                self.game.getPhase() == self.game.getInitial()):
+                # Start a new game
+                self.game.newGame(user)
+                msg = ("A new mafia game has begun! Type !join to join. There "
+                       "is a minimum of 5, and a maximum of 12 players that "
+                       "can play.\nThe game starter has been given Game "
+                       "Operator (GOP) status and can commence the game by "
+                       "typing !start.")
+
+            elif self.game.getPhase() != self.game.getInitial():
+                # Can't start a new game as one is already in progress
                 msg = ("A game is already in progress. Wait for it to finish "
                        "before starting a new one.\nIf you'd like to end a "
                        "game early, you can type !end to end the game, or "
                        "!restart to restart the game if you're the GOP.")
-                
+
             else:
-                #Can't start a new game, as we don't know what channel to play
-                #it in
+                # Can't start a new game, as we don't know what channel to play
+                # it in
                 msg = ("You can't start a game with a PM. Please try again "
                        "in the channel you'd like to play in.")
-            self.msg(channel, msg)
+            self.msg_send(self.nickname, channel, user, msg)
 
         elif command == "help":
-            #Help requested. Output usable commands.
-            #TODO - implement
+            # Help requested. Output usable commands.
+            # TODO - implement
             curr_phase = self.game.getPhase()
             if curr_phase == self.game.getInitial():
                 pass
@@ -164,32 +222,134 @@ class MafiaBot:
                 pass
             elif curr_phase == self.game.getNight():
                 pass
-            #TODO - also print out 'anytime' commands
+            # TODO - also print out 'anytime' commands
 
         elif command == "end":
-            #Attempt to end the game
-            if channel != self.nickname:
-                if self.game.end():
-                    #Success
+            # Attempt to end the game
+            if (channel != self.nickname.lower() and
+                self.game.getPhase() != self.game.getInitial()):
+                if self.game.end(user):
+                    # Success
                     msg = ("The game has been closed early by request of "
-                           + user + ". Thanks for playing!")
+                           + user_orig + ". Thanks for playing!")
                 else:
                     msg = ("Failed to end the game. Are you sure that a game "
                            "is running, and that you're the GOP?")
-            else:
+            elif channel == self.nickname.lower():
                 msg = ("This command cannot be sent via PM.")
-            self.msg(channel, msg)
+            else:
+                msg = ("You can't end a game that hasn't started!")
+            self.msg_send(self.nickname, channel, user, msg)
 
-        elif command == "":
+        elif command == "restart":
+            # Attempt to restart the game
+            restart = 0
+            if (channel != self.nickname.lower() and
+                self.game.getPhase() != self.game.getInitial()):
+                if self.game.end(user):
+                    # Success
+                    msg = ("The game has been closed early by request of "
+                           + user_orig + ". Thanks for playing!")
+                    restart = 1
+                else:
+                    msg = ("Failed to restart the game. Are you sure that a game "
+                           "is running, and that you're the GOP?")
+            elif channel == self.nickname.lower():
+                msg = ("This command cannot be sent via PM.")
+            else:
+                msg = ("You can't end a game that hasn't started!")
+            self.msg_send(self.nickname, channel, user, msg)
 
-        
+            if restart:
+                # Begin a new game
+                msg = "!new"
+                self.privmsg(user, channel, msg)
 
-            
-        #TODO - add more commands
+        elif command[:3] == "gop":
+            # Request to transfer GOP
+
+            # Check if a second argument has been provided
+            try:
+                target = command.split()[1]
+                target_orig = command_orig.split()[1]
+            except:
+                target = ""
+
+            if target and self.game.transferGop(user, target):
+                # Success
+                msg = ("GOP has been transferred from "
+                       "{} to {}.".format(user_orig, target_orig))
+            elif not target:
+                # Second argument not provided
+                msg = "Failed as no player name was provided."
+            else:
+                # Other failure
+                msg = ("Failed to transfer GOP. Are you sure you have GOP, "
+                        "and that the player '{}' exists?".format(target_orig))
+            self.msg_send(self.nickname, channel, user, msg)
+
+        elif command == "alive":
+            if self.game.getPhase() > self.game.getSign_Up():
+                # Game active, so determine living players
+                msg = "Players alive:"
+                for player in self.game.getLivingPlayers():
+                        msg += '\n' + player
+            else:
+                # Game inactive
+                msg = "No game is running."
+            self.msg_send(self.nickname, channel, user, msg)
+
+
+
+
+        # TODO - add more commands
 
 
     def irc_NICK(self, prefix, params):
         """Called when an IRC user changes their nickname."""
         old_nick = prefix.split('!')[0]
         new_nick = params[0]
-        #TODO - change name in player lists etc.
+        # TODO - change name in player lists etc.
+
+
+        # Other functions
+
+    def msg_send(self, nickname, channel, user, msg):
+        """Determines where to send a message, and sends it."""
+        if nickname.lower() == channel:
+            # Private message
+            self.msg(user, msg)
+        else:
+            # Channel
+            self.msg(channel, msg)
+
+
+class MafiaBotFactory(protocol.ClientFactory):
+    """A factory for MafiaBots."""
+
+    def __init__(self, channel):
+        self.channel = channel
+
+    def buildProtocol(self, addr):
+        bot = MafiaBot()
+        bot.factory = self
+        return bot
+
+    def clientConnectionLost(self, connector, reason):
+        """If we get disconnected, terminate the program."""
+        reactor.stop()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "connection failed:", reason
+        reactor.stop()
+
+
+if __name__ == '__main__':
+    # Create factory protocol and application
+    bot = MafiaBotFactory("mtest")
+
+    # Connect factory to this host and port
+    reactor.connectTCP("irc.vision-irc.net", 6667, bot)
+
+    # Run bot
+    reactor.run()
